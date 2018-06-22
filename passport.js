9068faf74772285d094d65d096411e39d813a9f4
Eloise Peng <eloise.k.p@gmail.com>
2018-06-22 15:31:10 -0700

    update passport setup

--- passport.js
+++ passport.js
@@ -1,8 +1,10 @@
+// user auth
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const Employees = require('./models/employee');
 
-passport.use(Employees.createStrategy());
-
+// passport config
+// passport.use(Employees.createStrategy());
+passport.use(new LocalStrategy(Employees.authenticate()));
 passport.serializeUser(Employees.serializeUser());
 passport.deserializeUser(Employees.deserializeUser());
